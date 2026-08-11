ad515b4f095dd91c112f259840a41c22a9aeae46
Omar Cavalieri <omarcavalieri@yahoo.com>
2026-08-11 15:05:49 -0300

    correcciones visuales segun validación

--- backend/routes/relevamientoroutes.js
+++ backend/routes/relevamientoroutes.js
@@ -6,10 +6,13 @@ import {
     actualizarRelevamiento,
     eliminarRelevamiento,
 } from '../controllers/relevamientocontroller.js';
+import { verificarToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // Definimos los verbos HTTP correspondientes
+router.get('/', verificarToken, obtenerrelevamientos);
+router.post('/', verificarToken, crearrelevamiento);
 router.get('/', obtenerrelevamientos);           // Listar todos: GET /api/relevamientos
 router.get('/:id', obtenerRelevamientoPorId);   // Obtener uno por ID: GET /api/relevamientos/:id
 router.post('/', crearrelevamiento);            // Crear nuevo: POST /api/relevamientos
