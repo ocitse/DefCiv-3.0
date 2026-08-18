8d95298af4b092a0ff24973b127b1b0ab4b17bf4
Omar Cavalieri <omarcavalieri@yahoo.com>
2026-08-18 03:30:08 -0300

    Implementando funcionalidad documentación

---
+++ backend/models/Documentacion.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); 
+
+const Documentacion = sequelize.define('Documentacion', {
+    id_documento: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    id_familia: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Familias',
+            key: 'id_familia'
+        },
+        onDelete: 'CASCADE' // Si se borra la familia, se borran sus adjuntos automáticamente
+    },
+    nombre_archivo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ruta_archivo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'documentacion_familias',
+    timestamps: true
+});
+
+module.exports = Documentacion;
